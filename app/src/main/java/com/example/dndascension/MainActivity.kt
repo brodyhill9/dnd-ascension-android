b4c6e801206b2cda127400d03f17ef41ecf365df
brodyhill9 <brodyhill827@gmail.com>
2020-03-08 20:34:51 -0600

    Added user authentication with drop-in UI from AWS Amplify. Created initial home page.

--- app/src/main/java/com/example/dndascension/MainActivity.kt
+++ app/src/main/java/com/example/dndascension/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.dndascension
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import com.amazonaws.mobile.client.AWSMobileClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        listView = findViewById(R.id.home_list_view)
+        val listItems = listOf("Characters", "Campaigns", "Assets")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -27,12 +34,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            Log.i(TAG, "Settings Action")
+        } else if (id == R.id.action_sign_out) {
+            Log.i(TAG, "Sign Out Action")
+            AWSMobileClient.getInstance().signOut()
+            val intent = Intent(applicationContext, AuthenticationActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            finish()
         }
+
+        return super.onOptionsItemSelected(item)
     }
 }
